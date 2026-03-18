c6bc52327d2090bf7f5d6cae32341f5670b40035
glarmer <hallrudfordlewis@gmail.com>
2026-03-18 02:30:59 +0000

    Add new options to the SetAllSettings() method

--- PEAK_Visuals/src/PEAK_Visuals/Settings.cs
+++ PEAK_Visuals/src/PEAK_Visuals/Settings.cs
@@ -19,6 +19,8 @@ public class Settings
         SetShadowDistance();
         SetShadowCascades();
         SetAnisotropicFiltering();
+        SetSoftShadows();
+        SetShadowmapResolution();
     }
 
     public void SetAllCameraSettings()
