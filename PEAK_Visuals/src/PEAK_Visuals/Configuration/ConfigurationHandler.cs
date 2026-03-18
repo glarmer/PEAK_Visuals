68f17e2507508840c87b12e83adfcb2fc375d516
glarmer <hallrudfordlewis@gmail.com>
2026-03-18 02:29:56 +0000

    Move to a config registration system less prone to human error

--- PEAK_Visuals/src/PEAK_Visuals/Configuration/ConfigurationHandler.cs
+++ PEAK_Visuals/src/PEAK_Visuals/Configuration/ConfigurationHandler.cs
@@ -1,5 +1,6 @@
 using System;
 using BepInEx.Configuration;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace PEAK_Visuals.Configuration;
@@ -17,12 +18,10 @@ public class ConfigurationHandler
     public ConfigEntry<int> ConfigShadowCascades;
     public ConfigEntry<int> ConfigShadowmapResolution;
     public ConfigEntry<bool> ConfigSoftShadows;
-    
     public ConfigEntry<bool> ConfigAnisotropicFiltering;
-    
     public ConfigEntry<int> ConfigCameraAA;
     public ConfigEntry<int> ConfigMSAA;
-    
+
     public float RenderScale => ConfigRenderScale.Value;
     public int UpscalingFilter => ConfigUpscalingFilter.Value;
     public float LodQuality => ConfigLODQuality.Value;
@@ -33,247 +32,137 @@ public class ConfigurationHandler
     public bool AnisotropicFiltering => ConfigAnisotropicFiltering.Value;
     public int CameraAA => ConfigCameraAA.Value;
     public int MSAA => ConfigMSAA.Value;
-    
+
     public ConfigurationHandler(ConfigFile configFile)
     {
         _config = configFile;
-        
+
         Plugin.Log.LogInfo("ConfigurationHandler initialising");
-        
-        
-        ConfigRenderScale = _config.Bind
-        (
+
+        ConfigRenderScale = Bind(
             "Scaling",
             "RenderScale",
             1f,
-            "Controls the render scale of the game. Native is 1.0 (100%). Range 0.1-2.0"
+            "Controls the render scale of the game. Native is 1.0 (100%). Range 0.1-2.0",
+            () => Plugin.Instance.Settings.SetResolutionScale(),
+            v => Mathf.Clamp(v, 0.1f, 2f)
         );
-        Plugin.Log.LogInfo("ConfigurationHandler: Render Scale set to: " + ConfigRenderScale.Value);
-        ConfigRenderScale.SettingChanged += OnRenderScaleChanged;
-        if (ConfigRenderScale.Value < 0.1f)
-        {
-            ConfigRenderScale.Value = 0.1f;
-        }
-        else if (ConfigRenderScale.Value > 2f)
-        {
-            ConfigRenderScale.Value = 2f;
-        }
-        
-        ConfigUpscalingFilter = _config.Bind
-        (
+
+        ConfigUpscalingFilter = Bind(
             "Scaling",
             "UpscalingFilter",
             1,
-            "Controls what filter the game uses to scale to your monitor resolution. 0 = auto, 1 = linear, 2 = point, 3 = FSR 1.0, 4 = STP"
+            "Controls what filter the game uses to scale to your monitor resolution. 0 = auto, 1 = linear, 2 = point, 3 = FSR 1.0, 4 = STP",
+            () => Plugin.Instance.Settings.SetUpscaler(),
+            v => Mathf.Clamp(v, 0, 4)
         );
-        Plugin.Log.LogInfo("ConfigurationHandler: Upscaling filter set to: " + ConfigUpscalingFilter.Value);
-        ConfigUpscalingFilter.SettingChanged += OnUpscalingFilterChanged;
-        if (ConfigUpscalingFilter.Value < 0)
-        {
-            ConfigUpscalingFilter.Value = 0;
-        }
-        else if (ConfigUpscalingFilter.Value > 4)
-        {
-            ConfigUpscalingFilter.Value = 4;
-        }
-        
-        ConfigLODQuality = _config.Bind
-        (
+
+        ConfigLODQuality = Bind(
             "LOD",
             "LODQuality",
             2.5f,
-            "Controls the LOD bias of the game. PEAK's High equates to 1.0. Higher values increase detail distance. Range 0.1-10"
+            "Controls the LOD bias of the game. PEAK's High equates to 1.0. Higher values increase detail distance. Range 0.1-10",
+            () => Plugin.Instance.Settings.SetLODQuality(),
+            v => Mathf.Clamp(v, 0.1f, 10f)
         );
-        Plugin.Log.LogInfo("ConfigurationHandler: LOD Quality set to: " + ConfigLODQuality.Value);
-        ConfigLODQuality.SettingChanged += OnLODQualityChanged;
-        if (ConfigLODQuality.Value < 0.1f)
-        {
-            ConfigLODQuality.Value = 0.1f;
-        }
-        else if (ConfigLODQuality.Value > 10f)
-        {
-            ConfigLODQuality.Value = 10f;
-        }
-        
-        ConfigShadowDistance = _config.Bind
-        (
+
+        ConfigShadowDistance = Bind(
             "Shadows",
             "ShadowDistance",
             200,
-            "Controls the maximum distance shadows are rendered. PEAK's High option equates to 200. Higher values improve distant shadows but reduce performance. Range 0-1000"
+            "Controls the maximum distance shadows are rendered. PEAK's High option equates to 200. Higher values improve distant shadows but reduce performance. Range 0-1000",
+            () => Plugin.Instance.Settings.SetShadowDistance(),
+            v => Mathf.Clamp(v, 0, 1000)
         );
-        Plugin.Log.LogInfo("ConfigurationHandler: Shadow Distance set to: " + ConfigShadowDistance.Value);
-        ConfigShadowDistance.SettingChanged += OnShadowDistanceChanged;
-        if (ConfigShadowDistance.Value < 0)
-        {
-            ConfigShadowDistance.Value = 0;
-        }
-        else if (ConfigShadowDistance.Value > 1000)
-        {
-            ConfigShadowDistance.Value = 1000;
-        }
-        
-        ConfigShadowCascades = _config.Bind
-        (
+
+        ConfigShadowCascades = Bind(
             "Shadows",
             "ShadowCascades",
             4,
-            "Controls the number of shadow cascades used by the directional light. PEAK's High option equates to 2. Higher values improve shadow stability. Range 1-10"
+            "Controls the number of shadow cascades used by the directional light. PEAK's High option equates to 2. Higher values improve shadow stability. Range 1-10",
+            () => Plugin.Instance.Settings.SetShadowCascades(),
+            v => Mathf.Clamp(v, 1, 4)
         );
-        Plugin.Log.LogInfo("ConfigurationHandler: Shadow Cascades set to: " + ConfigShadowCascades.Value);
-        ConfigShadowCascades.SettingChanged += OnShadowCascadesChanged;
-        if (ConfigShadowCascades.Value < 1)
-        {
-            ConfigShadowCascades.Value = 1;
-        }
-        else if (ConfigShadowCascades.Value > 10)
-        {
-            ConfigShadowCascades.Value = 10;
-        }
-        
-        ConfigShadowmapResolution = _config.Bind
-        (
+
+        ConfigShadowmapResolution = Bind(
             "Shadows",
             "ShadowmapResolution",
-            8192,
-            "Controls the quality of the shadows"
+            4096,
+            "Controls the quality of the shadows, can reduce performance so turn it down if you're having issues. Makes the trees less wobbly",
+            () => Plugin.Instance.Settings.SetShadowmapResolution(),
+            v => Mathf.Clamp(v, 1024, 20480)
         );
-        Plugin.Log.LogInfo("ConfigurationHandler: Shadowmap Resolution set to: " + ConfigShadowmapResolution.Value);
-        ConfigShadowmapResolution.SettingChanged += OnShadowmapResolutionChanged;
-        if (ConfigShadowmapResolution.Value < 0)
-        {
-            ConfigShadowmapResolution.Value = 0;
-        }
-        else if (ConfigShadowmapResolution.Value > 10240)
-        {
-            ConfigShadowmapResolution.Value = 10240;
-        }
-        
-        ConfigSoftShadows = _config.Bind
-        (
+
+        ConfigSoftShadows = Bind(
             "Shadows",
             "SoftShadows",
             true,
-            "Allows shadows to be soft, if your PC is too low spec for a high shadowmap setting this to false can stop the wobblyness of the shadows"
+            "Allows shadows to be soft, if your PC is too low spec for a high shadowmap setting this to false can stop the wobblyness of the shadows (but will result in a pixelly effect if the shadowmap res is low)",
+            () => Plugin.Instance.Settings.SetSoftShadows()
         );
-        Plugin.Log.LogInfo("ConfigurationHandler: Soft Shadows set to: " + ConfigSoftShadows.Value);
-        ConfigSoftShadows.SettingChanged += OnSoftShadowsChanged;
-        
-        ConfigAnisotropicFiltering = _config.Bind
-        (
+
+        ConfigAnisotropicFiltering = Bind(
             "Quality",
             "AnisotropicFiltering",
             true,
-            "Helps texture sharpness at angles (set to on)"
+            "Helps texture sharpness at angles (set to on)",
+            () => Plugin.Instance.Settings.SetAnisotropicFiltering()
         );
-        Plugin.Log.LogInfo("ConfigurationHandler: Anisotropic Filtering set to: " + ConfigAnisotropicFiltering.Value);
-        ConfigAnisotropicFiltering.SettingChanged += OnAnisotropicFilteringChanged;
-        
-        ConfigCameraAA = _config.Bind
-        (
+
+        ConfigCameraAA = Bind(
             "AntiAliasing",
             "CameraAA",
             2,
-            "Controls what type of AA the Camera uses. By default PEAK uses Temporal Antialiasing (TAA). All of these options are essentially clever blur filters and sometimes people find them unpleasant. 0 = None, 1 = FXAA, 2 = SMAA, 3 = TAA."
+            "Controls what type of AA the Camera uses. By default PEAK uses Temporal Antialiasing (TAA). All of these options are essentially clever blur filters and sometimes people find them unpleasant. 0 = None, 1 = FXAA, 2 = SMAA, 3 = TAA.",
+            () => Plugin.Instance.Settings.SetPostProcessAA(),
+            v => Mathf.Clamp(v, 0, 3)
         );
-        Plugin.Log.LogInfo("ConfigurationHandler: Camera AA set to: " + ConfigCameraAA.Value);
-        ConfigCameraAA.SettingChanged += OnCameraAAChanged;
-        if (ConfigCameraAA.Value < 0)
-        {
-            ConfigCameraAA.Value = 0;
-        }
-        else if (ConfigCameraAA.Value > 3)
-        {
-            ConfigCameraAA.Value = 3;
-        }
-        
-        ConfigMSAA = _config.Bind
-        (
+
+        ConfigMSAA = Bind(
             "AntiAliasing",
             "MSAA",
             8,
-            "Controls whether Multi-Sample Anti-Aliasing (MSAA) is enabled. MSAA smooths jagged edges on geometry by sampling pixels multiple times. It is generally sharper than post-process AA methods but only affects object edges and can slightly reduce performance. PEAK does not use it by default. Higher values = better quality but worse performance. Valid values = 0, 2, 4, 8."
+            "Controls whether Multi-Sample Anti-Aliasing (MSAA) is enabled. MSAA smooths jagged edges on geometry by sampling pixels multiple times. It is generally sharper than post-process AA methods but only affects object edges and can slightly reduce performance. PEAK does not use it by default. Higher values = better quality but worse performance. Valid values = 0, 2, 4, 8.",
+            () => Plugin.Instance.Settings.SetMSAA(),
+            v => Mathf.Clamp(v, 0, 8)
         );
-        Plugin.Log.LogInfo("ConfigurationHandler: MSAA set to: " + ConfigMSAA.Value);
-        ConfigMSAA.SettingChanged += OnMSAAChanged;
-        if (ConfigMSAA.Value < 0)
-        {
-            ConfigMSAA.Value = 0;
-        }
-        else if (ConfigMSAA.Value > 8)
-        {
-            ConfigMSAA.Value = 8;
-        }
-        
-        ConfigMenuKey = _config.Bind
-        (
+
+        ConfigMenuKey = Bind(
             "General",
             "Config Menu Key",
             "<Keyboard>/f11",
-            "Control path for opening the mod configuration menu (e.g. <Keyboard>/f2, <Keyboard>/space, <Keyboard>/escape)"
+            "Control path for opening the mod configuration menu (e.g. <Keyboard>/f2, <Keyboard>/space, <Keyboard>/escape)",
+            SetupInputAction
         );
-        Plugin.Log.LogInfo("ConfigurationHandler: Config Menu Key: " + ConfigMenuKey.Value);
+
         SetupInputAction();
-        ConfigMenuKey.SettingChanged += OnMenuKeyChanged;
-        
+
         Plugin.Log.LogInfo("ConfigurationHandler initialised");
     }
 
-    private void OnSoftShadowsChanged(object sender, EventArgs e)
+    private ConfigEntry<T> Bind<T>(string section, string key, T defaultValue, string description, Action onChanged = null, Func<T, T> clamp = null)
     {
-        Plugin.Instance.Settings.SetSoftShadows();
+        var entry = _config.Bind(section, key, defaultValue, description);
+
+        if (clamp != null) 
+            entry.Value = clamp(entry.Value);
+
+        Plugin.Log.LogInfo($"{key} set to: {entry.Value}");
+
+        if (onChanged != null)
+        {
+            entry.SettingChanged += (_, _) =>
+            {
+                if (clamp != null)
+                    entry.Value = clamp(entry.Value);
+
+                onChanged();
+            };
+        }
+
+        return entry;
     }
 
-    private void OnShadowmapResolutionChanged(object sender, EventArgs e)
-    {
-        Plugin.Instance.Settings.SetShadowmapResolution();
-    }
-
-    private void OnAnisotropicFilteringChanged(object sender, EventArgs e)
-    {
-        Plugin.Instance.Settings.SetAnisotropicFiltering();
-    }
-
-    private void OnCameraAAChanged(object sender, EventArgs e)
-    {
-        Plugin.Instance.Settings.SetPostProcessAA();
-    }
-
-    private void OnMSAAChanged(object sender, EventArgs e)
-    {
-        Plugin.Instance.Settings.SetMSAA();
-    }
-
-    private void OnRenderScaleChanged(object sender, EventArgs e)
-    {
-        Plugin.Instance.Settings.SetResolutionScale();
-    }
-    
-    private void OnUpscalingFilterChanged(object sender, EventArgs e)
-    {
-        Plugin.Instance.Settings.SetUpscaler();
-    }
-    
-    private void OnLODQualityChanged(object sender, EventArgs e)
-    {
-        Plugin.Instance.Settings.SetLODQuality();
-    }
-    
-    private void OnShadowDistanceChanged(object sender, EventArgs e)
-    {
-        Plugin.Instance.Settings.SetShadowDistance();
-    }
-
-    private void OnShadowCascadesChanged(object sender, EventArgs e)
-    {
-        Plugin.Instance.Settings.SetShadowCascades();
-    }
-    
-    private void OnMenuKeyChanged(object sender, EventArgs e)
-    {
-        SetupInputAction();
-    }
-    
     private void SetupInputAction()
     {
         MenuAction?.Dispose();
